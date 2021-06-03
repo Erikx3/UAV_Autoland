c7fd9c8c583eb031b3d37be1839cf2e97a595abe
eriks <e.suer@tu-berlin.de>
2021-06-03 09:03:06 +0200

    Add publisher subscriber example in one script

---
+++ src/example_python/example_python/subpub_member_function.py
@@ -0,0 +1,65 @@
+# Copyright 2016 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+        # Create publisher
+        self.publisher_ = self.create_publisher(String, 'topic_name2', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.topic2_callback)
+        # Variable for saving
+        self.submsg_str = ''
+
+    def listener_callback(self, msg):
+        self.submsg_str = msg.data
+        self.get_logger().info('I heard: "%s"' % msg.data)
+
+    def topic2_callback(self):
+        msg = String()
+        msg.data = 'Me Mirror: %s' % self.submsg_str
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
