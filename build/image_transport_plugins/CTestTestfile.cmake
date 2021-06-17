9af7e7c33252e19b2a5f45c8c022547c095113ce
Erik Suer <e.suer@campus.tu-berlin.de>
2021-06-17 08:16:12 +0000

    Only tracking src foulder of workspace for cleaner work

--- build/image_transport_plugins/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/specialtopics/src/image_transport_plugins/image_transport_plugins
-# Build directory: /home/ubuntu/specialtopics/build/image_transport_plugins
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
