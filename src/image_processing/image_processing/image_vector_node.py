bb33640da8061428219ef99806365def342ef6b5
eriks <e.suer@tu-berlin.de>
2021-06-21 14:21:59 +0200

    Add tvec and rvec detection and logger flags for first info

--- src/image_processing/image_processing/image_vector_node.py
+++ src/image_processing/image_processing/image_vector_node.py
@@ -37,7 +37,7 @@ class ImageVectors(Node):
             self.image_lis_callback,
             1)
         self.image_sub  # prevent unused variable warning
-        
+
         # Listening to camera info (should be static, for calibration parameters)
         self.camera_info_sub = self.create_subscription(
             CameraInfo,
@@ -45,7 +45,7 @@ class ImageVectors(Node):
             self.camera_info_lis_callback,
             1)
         self.camera_info_sub  # prevent unused variable warning
-        
+
         # Variable for saving active image data and camera info
         self.rgb_image_data = None
         self.rgb_image_cv = None
@@ -54,39 +54,54 @@ class ImageVectors(Node):
         self.trans_vec = None
         self.rot_vec = None
         self.quat_vec = None
-        
+
         # Init for Aruco markers
         self.aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
         self.aruco_parameters = aruco.DetectorParameters_create()  # Initialize Parameters
-        
+        self.marker_length_dict = {"1": 0.063,
+                                   "3": 0.063,
+                                   "5": 0.031,
+                                   "7": 0.063,
+                                   "9": 0.063}  # Dictionary for all 5 marker lengths
+
         # Create publisher
         self.publisher_ = self.create_publisher(Transform, 'image_vectors', 10)
-        	
+
+        # Some flags for logging
+        self.camera_info_lis_callback_flag = True
+        self.image_lis_callback_flag = True
+
     def camera_info_lis_callback(self, msg):
         self.camera_distortion = np.array(msg.d)
         self.camera_matrix = np.array(msg.k).reshape([3, 3])
-        
-        #self.get_logger().info('I received the camera info {} \n {}'.format(str(self.camera_distortion), str(self.camera_matrix)))
-        
+        if self.camera_info_lis_callback_flag:
+            self.get_logger().info(
+                'I received the camera info {} \n {}'.format(str(self.camera_distortion), str(self.camera_matrix)))
+            self.camera_info_lis_callback_flag = False
+
     def image_lis_callback(self, msg):
-        #self.rgb_image_data = msg.data
-        
+        # self.rgb_image_data = msg.data
+
         self.rgb_image_cv = CvBridge().imgmsg_to_cv2(msg, desired_encoding="rgb8")
         gray = cv2.cvtColor(self.rgb_image_cv, cv2.COLOR_BGR2GRAY)
-        
-        
-        #self.get_logger().info(str([str(gray), str(self.aruco_dict) , self.aruco_parameters]))  
-        #self.get_logger().info('I received the image: {}'.format(str(self.rgb_image_cv)))
-        
-        
+
+        # self.get_logger().info(str([str(gray), str(self.aruco_dict) , self.aruco_parameters]))
+        # self.get_logger().info('I received the image: {}'.format(str(self.rgb_image_cv)))
+
         corners, ids, rej = aruco.detectMarkers(gray, self.aruco_dict, parameters=self.aruco_parameters)
         if ids is not None:
-        	self.get_logger().info("I found {} aruco markers!".format(len(ids)))       		
-        
-        
-        #self.get_logger().info('I received the image from time {}'.format(str(msg.header.stamp)))
-        
-        
+            self.get_logger().info("I found {} aruco markers!".format(len(ids)))
+            for i, idt in enumerate(ids.flatten()):
+                marker_length = self.marker_length_dict[str(idt)]
+                if self.camera_matrix is not None and self.camera_distortion is not None:
+                    rvec, tvec, markerPoints = cv2.aruco.estimatePoseSingleMarkers(corners=corners[i],
+                                                                                   markerLength=marker_length,
+                                                                                   cameraMatrix=self.camera_matrix,
+                                                                                   distCoeffs=self.camera_distortion
+                                                                                   )
+
+        # self.get_logger().info('I received the image from time {}'.format(str(msg.header.stamp)))
+
         T = Transform()
         V = Vector3()
         Q = Quaternion()
@@ -99,9 +114,9 @@ class ImageVectors(Node):
         Q.w = 0.4
         T.translation = V
         T.rotation = Q
-                
+
         self.publisher_.publish(T)
-        #self.get_logger().info('I publish: ' + str(T))
+        # self.get_logger().info('I publish: ' + str(T))
 
 
 def main(args=None):
