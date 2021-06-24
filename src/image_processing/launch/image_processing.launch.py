8a2edeef95cf808a8acf2e9044b81828683391de
Erik Suer <e.suer@campus.tu-berlin.de>
2021-06-24 14:01:37 +0000

    Add launch file for starting node automatically

---
+++ src/image_processing/launch/image_processing.launch.py
@@ -0,0 +1,25 @@
+import launch
+import launch.actions
+import launch.substitutions
+import launch_ros.actions
+
+
+def generate_launch_description():
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='image_processing',
+            namespace='',
+            executable='image_vector_node',
+            output='screen',
+            name=['image_vectore_name']),
+        launch_ros.actions.Node(
+            package='v4l2_camera',
+            namespace='',
+            executable='v4l2_camera_node',
+            output='screen',
+            name=['camera_node']),
+        launch.actions.ExecuteProcess(
+        	cmd=['ros2', 'bag', 'record', '/image_raw', '/image_vectors'],
+        	output='screen'
+        	)
+    ])
